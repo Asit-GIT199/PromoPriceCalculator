2a5ea615b77d3481b0e1ba50619d97134babd762
Asit <asit199@gmail.com>
2020-07-03 12:52:58 +0530

    Unit price added for each product

---
+++ TotalPricePromoCalculator/ProductUnitPrice/UnitPrices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TotalPricePromoCalculator.ProductUnitPrice
+{
+    public class UnitPrices
+    {
+        public int UnitPriceforA { get; set; } = 50;
+        public int UnitPriceforB { get; set; } = 30;
+        public int UnitPriceforC { get; set; } = 20;
+        public int UnitPriceforD { get; set; } = 15;
+    }
+}
