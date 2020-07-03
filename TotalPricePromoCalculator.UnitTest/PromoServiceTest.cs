462598f1794e3b3922354b9503635c7780be9f93
Asit <asit199@gmail.com>
2020-07-03 13:55:06 +0530

    Adding 1st unit test

---
+++ TotalPricePromoCalculator.UnitTest/PromoServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TotalPricePromoCalculator.Enums;
+using TotalPricePromoCalculator.Models;
+using TotalPricePromoCalculator.Services;
+using Xunit;
+
+namespace TotalPricePromoCalculator.UnitTest
+{
+    public class PromotionServiceFixture
+    {
+        public TotalPriceCalculatorService priceCalculator = new TotalPriceCalculatorService();
+    }
+    public class PromoServiceTest : IClassFixture<PromotionServiceFixture>
+    {
+        private readonly PromotionServiceFixture _promotionServiceFixture;
+        public PromoServiceTest(PromotionServiceFixture promotionServiceFixture)
+        {
+            _promotionServiceFixture = promotionServiceFixture;
+        }
+
+        [Fact]
+        public void One_value_of_Product_ABC_shouldbe_the_sum_of_Unit_price()
+        {
+            var promo = _promotionServiceFixture.priceCalculator;
+            var mockOrder = new List<Product>()
+            {
+            new Product() { ProductId = ProductEnum.A, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.B, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.C, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.D, ProductQuantity = 1 }
+            };
+
+            var result = promo.TotalPriceCalculator(mockOrder);
+            Assert.Equal(110, result);
+        }
+    }
+}
