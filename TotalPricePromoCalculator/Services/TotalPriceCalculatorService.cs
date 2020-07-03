ea1fe355068a599dac409e646ffcc721ec0eb957
Asit <asit199@gmail.com>
2020-07-03 12:42:43 +0530

    Services folder added ITotalPriceCalculatorService inter face and TotalPriceCalculatorService method created

---
+++ TotalPricePromoCalculator/Services/TotalPriceCalculatorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TotalPricePromoCalculator.Services
+{
+    public class TotalPriceCalculatorService
+    {
+    }
+}
