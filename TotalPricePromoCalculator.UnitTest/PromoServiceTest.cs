b0e257027e1be42b39bb0d50fb92aa74edac5867
Asit <asit199@gmail.com>
2020-07-03 13:59:25 +0530

    Adding other Unit tests

--- TotalPricePromoCalculator.UnitTest/PromoServiceTest.cs
+++ TotalPricePromoCalculator.UnitTest/PromoServiceTest.cs
@@ -35,5 +35,52 @@ namespace TotalPricePromoCalculator.UnitTest
             var result = promo.TotalPriceCalculator(mockOrder);
             Assert.Equal(110, result);
         }
+        [Fact]
+        public void More_than_three_of_ProdcutA_will_apply_Promotionfor3A()
+        {
+            var promo = _promotionServiceFixture.priceCalculator;
+            var mockOrder = new List<Product>()
+            {
+            new Product() { ProductId = ProductEnum.A, ProductQuantity = 5 },
+            new Product() { ProductId = ProductEnum.B, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.C, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.D, ProductQuantity = 0 }
+            };
+
+            var result = promo.TotalPriceCalculator(mockOrder);
+            Assert.Equal(280, result);
+        }
+
+        [Fact]
+        public void More_than_two_of_ProdcutB_will_apply_Promotionfor2B()
+        {
+            var promo = _promotionServiceFixture.priceCalculator;
+            var mockOrder = new List<Product>()
+            {
+            new Product() { ProductId = ProductEnum.A, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.B, ProductQuantity = 5 },
+            new Product() { ProductId = ProductEnum.C, ProductQuantity = 0 },
+            new Product() { ProductId = ProductEnum.D, ProductQuantity = 1 }
+            };
+
+            var result = promo.TotalPriceCalculator(mockOrder);
+            Assert.Equal(185, result);
+        }
+
+        [Fact]
+        public void For_prodduct_CandD_both_exists_then_applu_PromotionforCnD()
+        {
+            var promo = _promotionServiceFixture.priceCalculator;
+            var mockOrder = new List<Product>()
+            {
+            new Product() { ProductId = ProductEnum.A, ProductQuantity = 5 },
+            new Product() { ProductId = ProductEnum.B, ProductQuantity = 5 },
+            new Product() { ProductId = ProductEnum.C, ProductQuantity = 1 },
+            new Product() { ProductId = ProductEnum.D, ProductQuantity = 1 }
+            };
+
+            var result = promo.TotalPriceCalculator(mockOrder);
+            Assert.Equal(380, result);
+        }
     }
 }
