9cdae5d1861cb352ed4a4e8516e4fa8474bcfc90
Asit <asit199@gmail.com>
2020-07-03 12:33:10 +0530

    PromoFactor and Promotion lists are added

---
+++ PromoLibrary/PromoFactors/PromotionFactor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromoLibrary.PromoFactors
+{
+    public class PromotionFactor
+    {
+    }
+}
---
+++ PromoLibrary/PromoLists/PromotionList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromoLibrary.PromoLists
+{
+    public class PromotionList
+    {
+        public int Promotionfor3A { get; set; } = 130;
+        public int Promotionfor2B { get; set; } = 45;
+        public int PromotionforCnD { get; set; } = 30;
+    }
+}
