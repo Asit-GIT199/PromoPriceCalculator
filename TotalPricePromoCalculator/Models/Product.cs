a8e8b22af85ba4084556d00202aba9d877d40d80
Asit <asit199@gmail.com>
2020-07-03 12:38:00 +0530

    Model folder added and product class is added

---
+++ TotalPricePromoCalculator/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TotalPricePromoCalculator.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
